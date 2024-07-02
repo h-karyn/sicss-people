ed49a3a184f31993337161847726a38c563dd13c
Yuanming Tao <ytao5219@gmail.com>
2024-07-02 14:21:10 +0800

    add code

--- sicss_scrping.py
+++ sicss_scrping.py
@@ -10,14 +10,13 @@ response = requests.get(url)
 # Check if the request was successful
 if response.status_code == 200:
     
-    # Parse the HTML content of the page
     soup = BeautifulSoup(response.content, 'html.parser')
     
     print(dir(soup))
-    # Find all elements that contain the names of people (adjust the selector as needed)
-    people_elements = soup.select('.body')  # Example selector, adjust based on actual HTML structure
+
+    people_elements = soup.select('.body') 
     print(people_elements)
-    # Extract and print the names
+
     for person in people_elements:
         name = person.get_text(strip=True)
         print(name)
